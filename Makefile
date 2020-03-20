3cd10acb6a96500bb7447891f5bfd0aa5509bd97
Tiemen Molenaar <tiemen131415@gmail.com>
2020-03-20 10:42:43 +0100

    Mijn eerste asm programa waar ik VFPv2 gebruikt namelijk VFPv2_PI.s

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 progs_gcc:=first sum01 sum02 sum03 load01 store01 
 progs_gcc+=branch01 branch02 compare01 loop01 loop02 collatz
 progs_gcc+=hello01 printf01 printf02 numericalLabels bsearch
-progs_gcc+=rfac predict_collatz
+progs_gcc+=rfac predict_collatz VFPv2_PI
 progs_ld:=sum04 load02 store02 store03 store04 branch03 addrmodes01 addrmodes02 addrmodes03
 progs_ld+=eigen01 syscall
 
@@ -17,7 +17,7 @@ $(progs_ld): $$@.o
 	ld -o $@ $+
 
 %.o: %.s
-	as -g -o $@ $^
+	as -g -o $@ $^ -mfpu=vfpv2
 
 .PHONY: clean
 clean:
---
+++ VFPv2_PI.s
@@ -0,0 +1,44 @@
+@ VFPv2_PI.s - test programa VFPv2 coprocessor
+@ benader het getal pi door 355/113 uit te rekenen 
+.balign 4
+.global main
+.global printf
+
+.text
+@ ---------- hoofd programa
+main:
+	stmdb sp!, {r4-r12, lr}
+	@@ Benader het getal pi	
+	@ laad variable a in register D1
+	ldr r0, =a
+	vldr D1, [r0]
+
+	@ laad variable b in register D2	
+	ldr r0, =b
+	vldr D2, [r0]
+
+	@ doe D0 <= D1 / D2
+	vdiv.f64 D0, D1, D2
+
+	@ print resultaat naar scherm
+	bl print
+
+	ldmia sp!, {r4-r12, 3lr}
+	bx lr
+
+@ print() - print de inoud van D0 register
+print:
+	stmdb sp!, {lr}
+	
+	@ print D0 register
+	ldr r0,=format
+	vmov r1,r2, D0
+	bl printf
+
+	ldmia sp!, {lr}
+	bx lr
+
+.data
+format: .asciz "%e\n"
+a: .double 355
+b: .double 113
